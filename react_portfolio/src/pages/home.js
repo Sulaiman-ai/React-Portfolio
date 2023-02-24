14664c2cd156365c5314b8c5752729099bdc8442
Sulaiman Amjad <sulaiman.amjad@gmail.com>
2023-02-24 16:38:51 +0000

    added 3 more projects data.json and added a bio and avatar

--- react_portfolio/src/pages/home.js
+++ react_portfolio/src/pages/home.js
@@ -4,9 +4,14 @@ function Home(){
     return (
         <div className="main">
         {/* Avatar */}
+        <img src={require("../icons/profile-pic.jpg")}></img>
         <h1>Sulaiman Amjad</h1>
         <div>
             <p>Front-end Web Developer</p>
+            <p>I am an enthusiastic and committed Software Engineer looking for roles in Full Stack Development.
+I have experience with Python, JavaScript, HTML, CSS, React SQL, Node and Express.
+I am passionate about problem solving, and using technology to improve peoples lives and provide efficient alternatives to deal with real life problems.
+My aim is to constantly improve my skills a learn new technologies and technical solutions.</p>
             <button>View CV</button>
             <Skills/>
             <p>Portfolio. Powered by ReactJS.</p>

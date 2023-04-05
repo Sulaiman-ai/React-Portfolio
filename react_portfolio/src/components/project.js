1fa48f4415d247f3d7b4f457cea127b4b8094d1f
Sulaiman Amjad <sulaiman.amjad@gmail.com>
2023-04-05 11:47:52 +0100

    Projects now in grid format. Needs editting.

--- react_portfolio/src/components/project.js
+++ react_portfolio/src/components/project.js
@@ -7,20 +7,20 @@ function Project(props){
     {/* <CBadge color="primary">NodeJS</CBadge> */}
     {/* <h1>Project 1</h1> */}
     <div className="project-container">
-    <img src={props.demosrc}/>
+    <img className="project-thumbnail" src={props.demosrc}/>
     <div className="project-details">
         <h2>{props.title}</h2>
         <div className="project-links">
-            <p><a href={props.repo} target="_blank">Source Code</a></p>
-            <p><a href={props.deployed} target="_blank">Application</a></p>
+            <div><a href={props.repo} target="_blank">GitHub</a></div>
+            <div><a href={props.deployed} target="_blank">View</a></div>
         </div>
-        <div className="project-description"></div>
-        <div className="project-skills">
+        {/* <div className="project-description"></div> */}
+        {/* <div className="project-skills"> */}
             {/* <Badge name="node.js"/> */}
             {/* {props.skills.map((e) => (
                 <CBadge color="primary">{e}</CBadge>
             ))} */}
-        </div>
+        {/* </div> */}
     </div>
     </div>
     </>
